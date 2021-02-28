a7e3e25ed5c0185a10b86af2079df6e376759866
ncerva <76188076+ncerva@users.noreply.github.com>
2021-02-28 11:47:02 -0600

    Javascript updated with Local Storage

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ var date = moment().format('MMM Do YYYY');
 var textarea = "text"
 var savebtn = "save"
 
-$("TodayDate").html(date);
+$("todayDate").html(date);
 
 //local storage
-// 
-localStorage.setItem(time,date)
+localStorage.setItem(time,date,textarea)
 
 // event listener for save button
+addEventListener.apply("click" + savebtn)
+
+// past present future color coded. if then statement 
+var timeBlock = ()
+if (timeBlock<time){
+
+}
+// if (current time < x = past color code it orange )
+// if current time >x = future color code it blue
