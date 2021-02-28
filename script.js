ad15740df434791c89e59578a6d65bea6f3b714d
ncerva <76188076+ncerva@users.noreply.github.com>
2021-02-28 16:46:31 -0600

    added code for past/present/future

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 //time and date
-//
-var time = moment().format(); 
-var date = moment().format('MMM Do YYYY');
+//current day displayed
 var textarea = "text"
 var savebtn = "save"
+var currentTime = moment().hour();
+var dateandtime = moment().format('MMMM Do YYYY, h:mm:ss a')
+$("#todayDate").html(date);
 
-$("todayDate").html(date);
-
+//prevent default
 //local storage
 localStorage.setItem(time,date,textarea)
 
 // event listener for save button
 addEventListener.apply("click" + savebtn)
 
+// connect it to the timer
 // past present future color coded. if then statement 
-var timeBlock = ()
+// if (current time < x = past color code it )
+if (timeBlock < currentTime) {
+    $(this).addclass("present");
+    $(this).removeclass("future");
+    $(this).removeclass("past");
+
+if (timeBlock < currentTime) {
+    $(this).addclass("past")
+    $(this).removeclass("future");
+    $(this).removeclass("present");
+
+else (timeBlock==now){
+    $(this).addclass("present")
+    $(this).removeclass("past")
+    $(this).removeclass("future")
+}
+}
+}
+// if current time >x = future color code it blue
+var timeBlock =
 if (timeBlock<time){
 
 }
-// if (current time < x = past color code it orange )
-// if current time >x = future color code it blue
+
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
